fb49c6621649e7ae93caae8051f9ea547d9397da
liamsmith3194 <smith.liam1994@gmail.com>
2022-05-10 18:18:24 +0000

    webhook handler completed but failing to process

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -32,7 +32,10 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-    
+
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
+
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
